a511dfa17f3b731363504163e9918b5a8da5e4a6
Bec Scott <me@becdetat.com>
2019-12-02 19:44:44 +1000

    fix title type in db

--- MigrateDatabase.go
+++ MigrateDatabase.go
@@ -6,5 +6,5 @@ import (
 
 func MigrateDatabase( db *sql.DB ) {
 	ExecuteStatement( db, "CREATE TABLE IF NOT EXISTS ticks (tick timestamp)" )
-	ExecuteStatement( db, "CREATE TABLE IF NOT EXISTS todos (id int, title string, position int, complete bool)" )
+	ExecuteStatement( db, "CREATE TABLE IF NOT EXISTS todos (id int, title text, position int, complete bool)" )
 }
