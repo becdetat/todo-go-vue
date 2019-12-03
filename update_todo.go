ff5f46374305d92343b39abfb8c1dd91698a6459
Bec Scott <me@becdetat.com>
2019-12-03 11:09:57 +1000

    fix update statement

--- update_todo.go
+++ update_todo.go
@@ -6,7 +6,7 @@ import (
 
 func UpdateTodo( db *sql.DB, id int, todo *Todo ) error {
 	_, err := db.Exec(
-		"UPDATE todos SET title = %2, position = %3, complete = %4 WHERE id = %1",
+		"UPDATE todos SET title = $2, position = $3, complete = $4 WHERE id = $1",
 		id,
 		todo.Title,
 		todo.Position,
