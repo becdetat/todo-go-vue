139da494aae9736ffd1b3bc8a6442ce9a52bcdaf
Bec Scott <me@becdetat.com>
2019-12-06 14:52:05 +1000

    reworked front-end, removed material UI, everything works

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <template>
   <div id="app">
-    <md-app>
-      <md-app-toolbar class="md-primary md-large">
-        <div class="md-toolbar-row">
-          <span class="md-title">Things to do</span>
-        </div>
-      </md-app-toolbar>
-
-      <md-app-content>
-        <TodoList/>
-      </md-app-content>
-    </md-app>
+    <h1>Things to do</h1>
+    <TodoList/>
   </div>
 </template>
 
@@ -28,10 +19,4 @@ export default {
 </script>
 
 <style>
-#app {
-  display: flex;
-}
-.md-app {
-  width: 100%;
-}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    <md-list>
-      <md-list-item v-for="item in items" v-bind:key="item.id">
-        <md-checkbox v-model="item.complete" v-on:change="updateTodo( item )"></md-checkbox>
-        <span class="md-list-item-text">
-          <span v-if="!item.complete">{{item.title}}</span>
-          <strike v-if="item.complete">{{item.title}}</strike>
-        </span>
-      </md-list-item>
-    </md-list>
-  </div>
+  <ul>
+    <li v-for="item in items" v-bind:key="item.id">
+      <input type="checkbox"
+             v-model="item.complete"
+             v-on:change="updateTodo( item )"/>
+      <span v-if="!item.editing">
+        <span v-if="!item.complete">{{item.title}}</span>
+        <strike v-if="item.complete">{{item.title}}</strike>
+      </span>
+      <button v-if="!item.editing"
+              v-on:click="startEditTodo( item )">
+        Edit
+      </button>
+      <div v-if="!!item.editing">
+        <input type="text" v-model="item.editedTitle"/>
+        <button v-on:click="saveEditTodo( item )">Save</button>
+        <button v-on:click="cancelEditTodo( item )">Cancel</button>
+      </div>
+    </li>
+    <li>
+      <label>
+        Add a new item:
+        <input type="text" v-model="newItemTitle"/>
+      </label>
+      <button v-on:click="createNewTodo()">Create</button>
+    </li>
+    {{items}}
+  </ul>
 </template>
 
 <script>
@@ -21,13 +38,18 @@ export default {
   name: 'TodoList',
   data: () => ( {
     items: [],
-    error: null
+    error: null,
+    newItemTitle: ''
   } ),
   mounted() {
     axios
       .get('https://thawing-bayou-17829.herokuapp.com/todos')
       .then( response => {
-        this.items = _.orderBy(response.data, x => x.position)
+        this.items = _.orderBy(response.data, x => x.position).map( x => ( {
+            ...x,
+            editing: false,
+            editedTitle: ''
+        } ) )
       } )
       .catch( error => {
         this.error = error
@@ -40,10 +62,50 @@ export default {
         .catch( error => {
           this.error = error
         } )
+    },
+    startEditTodo: ( todo ) => {
+      todo.editing = true
+      todo.editedTitle = todo.title
+    },
+    saveEditTodo: ( todo ) => {
+      todo.editing = false
+      todo.title = todo.editedTitle
+      axios
+        .put(`https://thawing-bayou-17829.herokuapp.com/todos/${todo.id}`, todo)
+        .catch( error => {
+          this.error = error
+        } )
+    },
+    cancelEditTodo: ( todo ) => {
+      todo.editing = false
+    },
+    createNewTodo() {
+      const todo = {
+        id: Math.max( ...this.items.map( x => x.id ) ) + 1,
+        title: this.newItemTitle,
+        position: Math.max( ...this.items.map( x => x.position ) ) + 1,
+        complete: false
+      }
+
+      this.items.push( todo )
+      this.newItemTitle = ''
+
+      axios
+        .post( 'https://thawing-bayou-17829.herokuapp.com/todos', todo )
+        .catch ( error => {
+          this.error = error
+        } )
     }
   }
 }
 </script>
 
 <style scoped>
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    display: flex;
+  }
 </style>
